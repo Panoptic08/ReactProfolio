f2252f02885c340ee177e9204f92923f4f4ca9d7
Panoptic08 <your_email@example.com>
2024-09-19 00:01:19 +0530

    add all

--- src/components/cp.jsx
+++ src/components/cp.jsx
@@ -11,25 +11,25 @@ function Cp() {
                 
             </div>
             <div className='text-blue-200 text-right mb-20'>
-                    <p className='text-2xl'>Total Questions Solved: 60</p>
+                    <p className='text-2xl'>Total Questions Solved: 111</p>
                 </div>
             <div className='flex justify-center items-end gap-10 mb-20'>
                 {/* Bar 1 */}
                 <div className='flex flex-col items-center transform hover:scale-105 transition-transform duration-300'>
                     <div className='bg-blue-500 w-20 h-64 rounded-t-lg'></div>
-                    <p className='text-blue-200 mt-2'>Arrays - 30</p>
+                    <p className='text-blue-200 mt-2'>Arrays - 70</p>
                 </div>
 
                 {/* Bar 2 */}
                 <div className='flex flex-col items-center'>
                     <div className='bg-blue-500 w-20 h-48 rounded-t-lg transform hover:scale-105 transition-transform duration-300'></div>
-                    <p className='text-blue-200 mt-2'>Strings - 20</p>
+                    <p className='text-blue-200 mt-2'>Strings - 30</p>
                 </div>
 
                 {/* Bar 3 */}
                 <div className='flex flex-col items-center'>
                     <div className='bg-blue-500 w-20 h-32 rounded-t-lg transform hover:scale-105 transition-transform duration-300'></div>
-                    <p className='text-blue-200 mt-2'>Trees - 10</p>
+                    <p className='text-blue-200 mt-2'>Trees - 11</p>
                 </div>
 
                 {/* Bar 4 */}
@@ -43,31 +43,15 @@ function Cp() {
                 {/* Profile 1 */}
                 <div className='flex flex-col items-center'>
                 <div className='h-20 w-20 bg-gray-400 rounded-full mb-2 transform hover:scale-105 transition-transform duration-300'>
-                        <a href="https://leetcode.com/Panoptic08/">
-                            <img src='https://upload.wikimedia.org/wikipedia/commons/1/19/LeetCode_logo_black.png' alt='LeetCode' className='h-12 rounded-full mt-5 ml-4' />
+                        <a href="https://codolio.com/profile/XMthRq4U">
+                            <img src='https://codolio.com/codolio_assets/gif-owl-transparent.GIF' alt='LeetCode' className='h-12 rounded-full mt-5 ml-4' />
                         </a>
                     </div>
-                    <p className='text-blue-200'>Panoptic08</p>
+                    <p className='text-blue-200'>kartik doda</p>
                 </div>
 
                 {/* Profile 2 */}
-                <div className='flex flex-col items-center'>
-                <div className='h-20 w-20 bg-gray-400 rounded-full mb-2 transform hover:scale-105 transition-transform duration-300'>
-                        <a href="https://www.naukri.com/code360/profile/WQ">
-                            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMgfbQtp9IayH6YmI4xDCj6mhJq32XzTTPWw&s' alt='LeetCode' className='h-12 rounded-full mt-5 ml-4' />
-                        </a>
-                    </div>
-                    <p className='text-blue-200'>panoptic</p>
-                </div>
-
-                <div className='flex flex-col items-center'>
-                <div className='h-20 w-20 bg-gray-400 rounded-full mb-2 transform hover:scale-105 transition-transform duration-300'>
-                        <a href="https://www.hackerrank.com/profile/Kartikdoda86">
-                            <img src='https://upload.wikimedia.org/wikipedia/commons/6/65/HackerRank_logo.png' />
-                        </a>
-                    </div>
-                    <p className='text-blue-200'>@Kartikdoda86</p>
-                </div>
+               
             </div>
 
             
